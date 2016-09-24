3e2c5d123ab6074f6f815520a866c8f7d08c6977
Daver Muzaffar <smuzzaf@L-SB8H3N6G8W-M.local>
2016-09-24 15:55:36 -0700

    adding js for toggle

--- scripts/mobile-pillars.js
+++ scripts/mobile-pillars.js
@@ -1,16 +1,19 @@
-var mobilePillerContainer = document.getElementsByClassName('pillars-section-2-mobile');
-console.log(mobilePillerContainer);
+var mobilePillarContainer = document.getElementById("pillars-mobile-container");
+var pillarContainerChildDivs = mobilePillarContainer.getElementsByTagName("div");
+
 function togglePillars(button)
 {
-  console.log(button);
-  button.setAttribute("class", "selected");
-  switch(button.id)
-  {
-    case "1":
-         button.value = "OFF";
-         break;
-    case "2":
-         button.value = "ON";
-         break;
+  for( i=0; i< pillarContainerChildDivs.length; i++ ) {
+   if (pillarContainerChildDivs[i].id === button.id){
+     pillarContainerChildDivs[i].setAttribute("class", "mobile-header-items selected");
+
+     document.getElementById(button.id + "-content").setAttribute("class", "show");
+   }else{
+     pillarContainerChildDivs[i].setAttribute("class", "mobile-header-items");
+     document.getElementById(pillarContainerChildDivs[i].id +'-content').setAttribute("class", "hide");
+   }
   }
 }
+
+// Init call to set mobile-core pillar to default
+togglePillars(document.getElementById("mobile-core"));
