5a16772066bdc22a58dc309c35662625cab3ceba
tiffine-koch <tiffine.koch@gmail.com>
2016-09-28 16:01:43 -0700

    Updats mob-header with WalmartLogo on nav

--- scripts/mob-nav.js
+++ scripts/mob-nav.js
@@ -6,9 +6,11 @@ function changeMobileNav(){
   if(dropDownMenuState){
     document.getElementById("electrode-nav").style.backgroundColor = "#000e23";
     document.getElementById("mob-header-icon").setAttribute("src", "/img/mob-close.svg");
+    document.getElementById("walmart-header-logo").style.display = "none";
   } else {
     document.getElementById("electrode-nav").style.backgroundColor = "#00294b";
     document.getElementById("mob-header-icon").setAttribute("src", "/img/mob-menu.svg");
+    document.getElementById("walmart-header-logo").style.display = "block";
   }
 };
 
