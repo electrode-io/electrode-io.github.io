88bd32dabe373f0c2f2a5cb0af5ddc9235571609
Daver Muzaffar <smuzzaf@L-SB8H3N6G8W-M.local>
2016-09-28 15:45:27 -0700

    fixish arrow work

--- scripts/mobile-pillars.js
+++ scripts/mobile-pillars.js
@@ -9,11 +9,13 @@ function togglePillars(pillarSelection) {
       document.getElementById("mobile-"+ p).setAttribute("class", "mobile-header-items selected");
       document.getElementById("mobile-" + p + "-content").setAttribute("class", " show");
       document.getElementById("mobile-" + p + "-footer").style.color = "#ff0076";
+      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items footer-arrow-up");
       document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full@3x.png");
     }else{
       document.getElementById("mobile-"+ p).setAttribute("class", "mobile-header-items");
       document.getElementById("mobile-" + p + "-content").setAttribute("class", "hide");
       document.getElementById("mobile-" + p + "-footer").style.color = "#333";
+      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items");
       document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full-2@3x.png");
     }
   });
