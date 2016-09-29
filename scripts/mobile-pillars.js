9a28730a26740f02c7566ee1dbaa3c7ec7cb59cf
Daver Muzaffar <smuzzaf@L-SB8H3N6G8W-M.local>
2016-09-29 13:29:20 -0700

    init slick carousel

--- scripts/mobile-pillars.js
+++ scripts/mobile-pillars.js
@@ -7,13 +7,12 @@ function togglePillars(pillarSelection) {
   pillarsArr.forEach(function(p){
     if (pillarSelection === p){
       document.getElementById("mobile-"+ p).setAttribute("class", "mobile-header-items selected");
-      document.getElementById("mobile-" + p + "-content").setAttribute("class", " show");
+      $('.slickCarousel').slick('slickGoTo', pillarsArr.indexOf(p));
       document.getElementById("mobile-" + p + "-footer").style.color = "#ff0076";
       document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items footer-arrow-up");
       document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full@3x.png");
     }else{
       document.getElementById("mobile-"+ p).setAttribute("class", "mobile-header-items");
-      document.getElementById("mobile-" + p + "-content").setAttribute("class", "hide");
       document.getElementById("mobile-" + p + "-footer").style.color = "#333";
       document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items");
       document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full-2@3x.png");
@@ -22,4 +21,4 @@ function togglePillars(pillarSelection) {
 };
 
 // Init call to set mobile-core pillar as default
-togglePillars("core");
+//togglePillars("core");
