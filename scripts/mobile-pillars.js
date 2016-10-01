ad5fccaf203d53c617dc123e19063476223f0e9b
Daver Muzaffar <smuzzaf@daver.local>
2016-09-30 21:13:28 -0700

    make switching mobile page more efficent

--- scripts/mobile-pillars.js
+++ scripts/mobile-pillars.js
@@ -1,4 +1,7 @@
-// toggle function to toggle currently selected pillar in mobile view
+// State
+var pillarsArr = ["core", "modules", "tools"];
+var pillarsObject = {"core": "2%", "modules": "36%", "tools": "69%"};
+var pillarsState = {"core": true, "modules": false, "tools": false};
 
 // Init Slick carousel
 $(document).ready(function(){
@@ -12,42 +15,34 @@ $(document).ready(function(){
   togglePillars("core");
 });
 
-
-var pillarsArr = ["core", "modules", "tools"];
-var pillarsObject = {"core": "2%", "modules": "36%", "tools": "69%"};
-
+// handle click of moble toggle
 function togglePillars(pillarSelection) {
-
-  pillarsArr.forEach(function(p){
-    if (pillarSelection === p){
-      document.getElementById("slider-toggle").style.marginLeft = pillarsObject[p];
-      $(".slickCarousel").slick("slickGoTo", pillarsArr.indexOf(p));
-      document.getElementById("mobile-" + p + "-footer").style.color = "#ff0076";
-      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items footer-arrow-up");
-      document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full@3x.png");
-    }else{
-      document.getElementById("mobile-" + p + "-footer").style.color = "#333";
-      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items");
-      document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full-2@3x.png");
-    }
-  });
+  console.log("TOGGLE-TOGGLE");
+  $(".slickCarousel").slick("slickGoTo", pillarsArr.indexOf(pillarSelection));
 };
 
+//handle change/update of carousel
 function updateToggleMenu(currentSlide){
+  console.log("UPDATE-TOGGLE");
   pillarsArr.forEach(function(p, index){
-    if (currentSlide === index){
+    if (currentSlide === index && pillarsState[p] === false){
+      pillarsState[p] = true;
+      document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full@3x.png");
       document.getElementById("mobile-" + p + "-footer").style.color = "#ff0076";
       document.getElementById("slider-toggle").style.marginLeft = pillarsObject[p];
       document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items footer-arrow-up");
-      document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full@3x.png");
-    }else{
+    }else if (currentSlide != index && pillarsState[p] === true) {
+      pillarsState[p] = false;
+      document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full-2@3x.png");
       document.getElementById("mobile-" + p + "-footer").style.color = "#333";
       document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items");
-      document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full-2@3x.png");
+    }else {
+      console.log("Skipped: ", p);
     }
   });
 }
 
-$(".slickCarousel").on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+// listen for changes
+$(".slickCarousel").on('afterChange', function (event, slick, currentSlide, nextSlide) {
     updateToggleMenu(currentSlide);
 });
