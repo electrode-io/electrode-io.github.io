938c60d7b55d292258e6b521a053cb39b72f3c31
Daver Muzaffar <smuzzaf@daver.local>
2016-10-02 15:45:55 -0700

    no get calls for toggle img, replace dom elements instead

--- scripts/mobile-pillars.js
+++ scripts/mobile-pillars.js
@@ -7,13 +7,86 @@ var pillarsState = {"core": false, "modules": false, "tools": false};
 var outerMostSlickElement;
 var mobileToolsConent;
 
+// make array of img dom elements
+// rather than make get calls for toggeling imgs, replace dom element.
+var bluePillarImgArray = new Array();
 
-var my_image2 = new Image();
-my_image2.src = "/img/core-full@3x.png";
-var my_image3 = new Image();
-my_image3.src = "/img/modules-full@3x.png";
-var my_image3 = new Image();
-my_image3.src = "/img/tools-full@3x.png";
+bluePillarImgArray[0] = new Image();
+bluePillarImgArray[0].src = "/img/core-full@3x.png";
+bluePillarImgArray[0].setAttribute("id", "mobile-core-pillar");
+bluePillarImgArray[0].setAttribute("onClick", "togglePillars('core')");
+
+bluePillarImgArray[1] = new Image();
+bluePillarImgArray[1].src = "/img/modules-full@3x.png";
+bluePillarImgArray[1].setAttribute("id", "mobile-modules-pillar");
+bluePillarImgArray[1].setAttribute("onClick", "togglePillars('modules')");
+
+bluePillarImgArray[2] = new Image();
+bluePillarImgArray[2].src = "/img/tools-full@3x.png";
+bluePillarImgArray[2].setAttribute("id", "mobile-tools-pillar");
+bluePillarImgArray[2].setAttribute("onClick", "togglePillars('tools')");
+
+var redPillarImgArray = new Array();
+
+redPillarImgArray[0] = new Image();
+redPillarImgArray[0].src = "/img/core-full-2@3x.png";
+redPillarImgArray[0].setAttribute("id", "mobile-core-pillar");
+redPillarImgArray[0].setAttribute("onClick", "togglePillars('core')");
+
+redPillarImgArray[1] = new Image();
+redPillarImgArray[1].src = "/img/modules-full-2@3x.png";
+redPillarImgArray[1].setAttribute("id", "mobile-modules-pillar");
+redPillarImgArray[1].setAttribute("onClick", "togglePillars('modules')");
+
+redPillarImgArray[2] = new Image();
+redPillarImgArray[2].src = "/img/tools-full-2@3x.png";
+redPillarImgArray[2].setAttribute("id", "mobile-tools-pillar");
+redPillarImgArray[2].setAttribute("onClick", "togglePillars('tools')");
+
+// handle click of mobile toggle
+function togglePillars(pillarSelection, shouldScrollTop) {
+  if(!!shouldScrollTop) scrollToDiv();
+  $(".slickCarousel").slick("slickGoTo", pillarsArr.indexOf(pillarSelection));
+}
+
+//nested carousel right/left functions
+function nestedSlidePrev() {
+  $(".carousel-nested").slick('slickPrev');;
+}
+function nestedSlideForw() {
+  $(".carousel-nested").slick('slickNext');;
+}
+
+//handle change/update of carousel
+function updateToggleMenu(currentSlide) {
+  pillarsArr.forEach(function(p, index) {
+    if (currentSlide === index && pillarsState[p] === false) {
+      pillarsState[p] = true;
+      $("#mobile-" + p + "-pillar").replaceWith(bluePillarImgArray[currentSlide]);
+      document.getElementById("mobile-" + p + "-footer").style.color = "#ff0076";
+      document.getElementById("slider-toggle").style.marginLeft = pillarsObject[p];
+      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items footer-arrow-up");
+    } else if (currentSlide != index && pillarsState[p] === true) {
+      pillarsState[p] = false;
+      $("#mobile-" + p + "-pillar").replaceWith(redPillarImgArray[index]);
+      document.getElementById("mobile-" + p + "-footer").style.color = "#333";
+      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items");
+    }
+  });
+}
+
+function scrollToDiv() {
+  $('html,body').animate({scrollTop: $("#pillars-mobile-container").offset().top},'slow');
+}
+
+// listen for changes
+$(".slickCarousel").on('afterChange', function (event, slick, currentSlide, nextSlide) {
+  if(slick.options.parent){
+    updateToggleMenu(currentSlide, false);
+  }else if (!slick.options.parent){
+    outerMostSlickElement.style.height = mobileToolsContent.clientHeight+"px";
+  }
+});
 
 // Init Slick carousel
 $(document).ready(function() {
@@ -39,48 +112,4 @@ $(document).ready(function() {
   });
   outerMostSlickElement = document.getElementsByClassName("slick-list draggable")[0];
   mobileToolsContent = document.getElementById("mobile-tools-content");
-});
-
-// handle click of moble toggle
-function togglePillars(pillarSelection, shouldScrollTop) {
-  if(!!shouldScrollTop) scrollToDiv();
-  $(".slickCarousel").slick("slickGoTo", pillarsArr.indexOf(pillarSelection));
-}
-
-function nestedSlidePrev() {
-  $(".carousel-nested").slick('slickPrev');;
-}
-function nestedSlideForw() {
-  $(".carousel-nested").slick('slickNext');;
-}
-
-//handle change/update of carousel
-function updateToggleMenu(currentSlide) {
-  pillarsArr.forEach(function(p, index) {
-    if (currentSlide === index && pillarsState[p] === false) {
-      pillarsState[p] = true;
-      document.getElementById("mobile-" + p + "-pillar").src = "/img/" + p + "-full@3x.png";
-      document.getElementById("mobile-" + p + "-footer").style.color = "#ff0076";
-      document.getElementById("slider-toggle").style.marginLeft = pillarsObject[p];
-      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items footer-arrow-up");
-    } else if (currentSlide != index && pillarsState[p] === true) {
-      pillarsState[p] = false;
-      document.getElementById("mobile-" + p + "-pillar").setAttribute("src", "/img/" + p + "-full-2@3x.png");
-      document.getElementById("mobile-" + p + "-footer").style.color = "#333";
-      document.getElementById("mobile-" + p + "-footer").setAttribute("class", "elc-icon-icn-" + p + " footer-toggle-items");
-    }
-  });
-}
-
-function scrollToDiv() {
-  $('html,body').animate({scrollTop: $("#pillars-mobile-container").offset().top},'slow');
-}
-
-// listen for changes
-$(".slickCarousel").on('afterChange', function (event, slick, currentSlide, nextSlide) {
-  if(slick.options.parent){
-    updateToggleMenu(currentSlide, false);
-  }else if (!slick.options.parent){
-    outerMostSlickElement.style.height = mobileToolsContent.clientHeight+"px";
-  }
 });
