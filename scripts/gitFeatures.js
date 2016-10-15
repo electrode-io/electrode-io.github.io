012a66bcbf2ed8a0adc5caac48b4fa65bb8a86bf
Arpan Nanavati <nanavatiarpan@gmail.com>
2016-10-14 17:43:21 -0700

    new version Fri Oct 14 17:43:21 PDT 2016

---
+++ scripts/gitFeatures.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function GitFeatures() {
+  this.repos = this.getRepos();
+  this.issueHeaderId = "help-features";
+  this.issueListIdPrefix = "feature-list-";
+}
+
+GitFeatures.prototype = Object.create(GitIssues.prototype);
+GitFeatures.prototype.constructor = GitFeatures;
+
+GitFeatures.prototype.render = function(repo) {
+  var self = this;
+
+  self.getData(repo).then(function(issues) {
+    issues = issues.filter(enhancementFilter);
+
+    if (issues.length === 0) {
+      return;
+    }
+
+    self.appendIssueHeader(repo, issues);
+    self.renderIssueList(repo, issues);
+
+    function enhancementFilter(issue) {
+      return issue.labels.find(findEnhancement);
+    }
+
+    function findEnhancement (label) {
+      return label.name === "enhancement" || label.name === "help wanted";
+    }
+  });
+};
+
+var features = new GitFeatures();
+features.run();
